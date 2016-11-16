12eeb52c33c898e074732fa7df49126f0f7b924f
Donovan Allen <dallen.928@gmail.com>
2016-11-15 19:50:19 -0500

    add homepage, styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "artists#index"
+  root "main#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :artists do
